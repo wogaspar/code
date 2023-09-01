2198e83d07276c53e64a4246a40bc1fa9dcec18f
walter gaspar <wogaspar@yahoo.com>
2023-09-01 17:04:24 -0300

    fixed tests

--- test_repository.py
+++ test_repository.py
@@ -1,6 +1,7 @@
 # pylint: disable=protected-access
 import model
 import repository
+from sqlalchemy import text
 
 
 def test_repository_can_save_a_batch(session):
@@ -10,44 +11,30 @@ def test_repository_can_save_a_batch(session):
     repo.add(batch)
     session.commit()
 
-    rows = session.execute(
-        'SELECT reference, sku, _purchased_quantity, eta FROM "batches"'
-    )
+    rows = session.execute(text('SELECT reference, sku, _purchased_quantity, eta FROM "batches"'))
+
     assert list(rows) == [("batch1", "RUSTY-SOAPDISH", 100, None)]
 
 
 def insert_order_line(session):
-    session.execute(
-        "INSERT INTO order_lines (orderid, sku, qty)"
-        ' VALUES ("order1", "GENERIC-SOFA", 12)'
-    )
-    [[orderline_id]] = session.execute(
-        "SELECT id FROM order_lines WHERE orderid=:orderid AND sku=:sku",
-        dict(orderid="order1", sku="GENERIC-SOFA"),
-    )
+    session.execute(text('INSERT INTO order_lines (orderid, sku, qty) VALUES ("order1", "GENERIC-SOFA", 12)'))
+    
+    [[orderline_id]] = session.execute(text('SELECT id FROM order_lines WHERE orderid=:orderid AND sku=:sku'), dict(orderid="order1", sku="GENERIC-SOFA"))
+    
     return orderline_id
 
 
 def insert_batch(session, batch_id):
-    session.execute(
-        "INSERT INTO batches (reference, sku, _purchased_quantity, eta)"
-        ' VALUES (:batch_id, "GENERIC-SOFA", 100, null)',
-        dict(batch_id=batch_id),
-    )
-    [[batch_id]] = session.execute(
-        'SELECT id FROM batches WHERE reference=:batch_id AND sku="GENERIC-SOFA"',
-        dict(batch_id=batch_id),
-    )
+    session.execute(text('INSERT INTO batches (reference, sku, _purchased_quantity, eta) VALUES (:batch_id, "GENERIC-SOFA", 100, null)'), dict(batch_id=batch_id))
+    
+    [[batch_id]] = session.execute(text('SELECT id FROM batches WHERE reference=:batch_id AND sku="GENERIC-SOFA"'), dict(batch_id=batch_id))
+    
     return batch_id
 
 
 def insert_allocation(session, orderline_id, batch_id):
-    session.execute(
-        "INSERT INTO allocations (orderline_id, batch_id)"
-        " VALUES (:orderline_id, :batch_id)",
-        dict(orderline_id=orderline_id, batch_id=batch_id),
-    )
-
+    session.execute(text('INSERT INTO allocations (orderline_id, batch_id) VALUES (:orderline_id, :batch_id)'), dict(orderline_id=orderline_id, batch_id=batch_id))
+    
 
 def test_repository_can_retrieve_a_batch_with_allocations(session):
     orderline_id = insert_order_line(session)
